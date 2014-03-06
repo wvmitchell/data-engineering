f280a083122b059e1c4ed65109730c8bfb47964a
Will Mitchell <wvmitchell@gmail.com>
2014-03-06 10:37:55 -0700

    added dependencies to gemfile

---
+++ tab_parser/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TabParser::Application.config.secret_key_base = 'ef1534f952395cbf7a85b08f12b1fa94d9df09793b658f4daa1f7453834a2443dd1653b47252427b2dace44d3eb02072dd4abfa4b3b4a9e29e1bab5d9e26c177'
---
+++ tab_parser/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TabParser::Application.config.session_store :cookie_store, key: '_tab_parser_session'
