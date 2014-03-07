aaedef5637a83f529dc4bf429b10afba6816e28b
Will Mitchell <wvmitchell@gmail.com>
2014-03-07 11:17:52 -0700

    making use of uploaded tsv

--- tab_parser/app/controllers/dashboard_controller.rb
+++ tab_parser/app/controllers/dashboard_controller.rb
@@ -1,9 +1,17 @@
+require 'csv'
 class DashboardController < ApplicationController
 
   def new
   end
 
   def create
+    file = params["Deals"]
+    parsed = CSV.parse(file.read, {col_sep: "\t", headers: true})
+    parsed.each do |row|
+      deal = Deal.new
+      deal.purchaser = row["purchaser name"]
+      deal.save
+    end
     redirect_to dashboard_index_path
   end
 
--- tab_parser/spec/features/user_can_load_form_spec.rb
+++ tab_parser/spec/features/user_can_load_form_spec.rb
@@ -13,4 +13,19 @@ describe 'uploading a form' do
     page.should have_content('Gross Revenue')
   end
 
+  it 'takes data from the form' do
+    visit root_path
+
+    within('#upload_form') do
+      attach_file('Deals', '../example_input.tab')
+      click_on 'Submit'
+    end
+
+    within('#purchasers') do
+      page.body.should include('Snake')
+      page.body.should include('Marty')
+      page.body.should include('Amy')
+    end
+  end
+
 end
