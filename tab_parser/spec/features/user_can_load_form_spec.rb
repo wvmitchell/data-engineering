111ba9bbe8da9b66fa2403e60d1fd6be3cf4c3ff
Will Mitchell <wvmitchell@gmail.com>
2014-03-07 09:57:38 -0700

    added feature test for uploading form

---
+++ tab_parser/spec/features/user_can_load_form_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'uploading a form' do
+
+  it 'can accept a form' do
+
+    visit root_path
+
+    within('#upload_form') do
+      attach_file('Deals', '../example_input.tab')
+      click_on 'Submit'
+    end
+
+    expect(page.status).to eq(200)
+
+  end
+
+end
